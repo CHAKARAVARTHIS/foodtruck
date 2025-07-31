0aaeeb6f2719a71835070503b53f5467f5a13618
CHAKARAVARTHIS <chakaravarthikumar@gmail.com>
2025-07-31 07:27:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let togglemenu = document.getElementById('mobiletoggle');
+            let menulink = document.getElementById('menu');
+            togglemenu.addEventListener("click",()=>{
+                menulink.classList.toggle('open');
+                // menulink.classList.add('bx-x');
+            })
+
+
+            
